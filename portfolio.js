e4ecd567df4270e744601c2c89dea9608894d497
work4m <workingformix@gmail.com>
2022-01-18 18:39:00 +0530

    first commit

---
+++ portfolio.js
@@ -0,0 +1,350 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+	name: "BitSpark Solutions",
+	title: "Hi all, We are BitSpark",
+	description:
+		"We are a leading custom software development company providing world-magnificence software program development offerings primarily based totally on 5+ years of experience, a group of certified professionals, key enterprise insights, and dedicated workflows.",
+	// resumeLink: "https://cv.BitSpark.ga",
+};
+
+export const openSource = {
+	githubUserName: "work4m",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+	// facebook: "https://www.facebook.com/work4m",
+	// instagram: "https://www.instagram.com/work4m",
+	// twitter: "https://twitter.com/work4m",
+	// github: "https://github.com/work4m",
+	linkedin: "https://www.linkedin.com/company/bitspark-solutions",
+};
+
+export const skillsSection = {
+	title: "What I do",
+	subTitle:
+		"CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+	skills: [
+		emoji(
+			"⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+		),
+		emoji(
+			"⚡ We are developing Web apps for various industry, we have experienced of E-commerce, CRM, CMS & any type of Customized Web apps."
+		),
+		emoji(
+			"⚡ We have creative UI/UX designers, they help you to design Innovative and clean design of your apps according to your requirement."
+		),
+		emoji(
+			"⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"
+		),
+		emoji(
+			"⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+		),
+	],
+
+	softwareSkills: [
+		{
+			skillName: "html-5",
+			fontAwesomeClassname: "vscode-icons:file-type-html",
+		},
+		{
+			skillName: "css3",
+			fontAwesomeClassname: "vscode-icons:file-type-css",
+		},
+		{
+			skillName: "sass",
+			fontAwesomeClassname: "logos:sass",
+		},
+		{
+			skillName: "JavaScript",
+			fontAwesomeClassname: "logos:javascript",
+		},
+		{
+			skillName: "TypeScript",
+			fontAwesomeClassname: "logos:typescript-icon",
+		},
+		{
+			skillName: "reactjs",
+			fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+		},
+		{
+			skillName: "nodejs",
+			fontAwesomeClassname: "logos:nodejs-icon",
+		},
+		{
+			skillName: "flutter",
+			fontAwesomeClassname: "logos:flutter",
+		},
+		{
+			skillName: "ReactNative",
+			fontAwesomeClassname: "tabler:brand-react-native",
+		},
+		{
+			skillName: "swift",
+			fontAwesomeClassname: "vscode-icons:file-type-swift",
+		},
+		{
+			skillName: "npm",
+			fontAwesomeClassname: "vscode-icons:file-type-npm",
+		},
+		{
+			skillName: "sql-database",
+			fontAwesomeClassname: "vscode-icons:file-type-sql",
+		},
+		{
+			skillName: "mongoDB",
+			fontAwesomeClassname: "vscode-icons:file-type-mongo",
+		},
+		{
+			skillName: "aws",
+			fontAwesomeClassname: "logos:aws",
+		},
+		{
+			skillName: "firebase",
+			fontAwesomeClassname: "logos:firebase",
+		},
+		{
+			skillName: "python",
+			fontAwesomeClassname: "logos:python",
+		},
+		{
+			skillName: "git",
+			fontAwesomeClassname: "logos:git-icon",
+		},
+		{
+			skillName: "docker",
+			fontAwesomeClassname: "logos:docker-icon",
+		},
+	],
+};
+
+export const SkillBars = [
+	{
+		Stack: "Frontend/Design", //Insert stack or technology you have experience in
+		progressPercentage: "90", //Insert relative proficiency in percentage
+	},
+	{
+		Stack: "Backend",
+		progressPercentage: "70",
+	},
+	{
+		Stack: "Programming",
+		progressPercentage: "60",
+	},
+	{
+		Stack: "UI/UX designers",
+		progressPercentage: "75",
+	},
+];
+
+export const educationInfo = [
+	{
+		schoolName: "Harvard University",
+		subHeader: "Master of Science in Computer Science",
+		duration: "September 2017 - April 2019",
+		desc: "Participated in the research of XXX and published 3 papers.",
+		descBullets: [
+			"Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit",
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		],
+	},
+	{
+		schoolName: "Harvard",
+		subHeader: "Master of Science in Computer Science",
+		duration: "September 2017 - April 2019",
+		desc: "Participated in the research of XXX and published 3 papers.",
+		descBullets: [
+			"Lorem ipsum dolor sit amet, consectetfgur adipiscing elit",
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+			""
+		],
+	},
+	{
+		schoolName: "Stanford University",
+		subHeader: "Bachelor of Science in Computer Science",
+		duration: "September 2013 - April 2017",
+		desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
+		descBullets: [
+			"Lorem ipsum dolorfdg sit amet, consectetur adipiscing elit",
+		],
+	},
+];
+
+export const experience = [
+	{
+		role: "Software Engineer",
+		company: "Google",
+		companylogo: "/img/icons/common/google.svg",
+		date: "June 2018 – Present",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		descBullets: [
+			"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		],
+	},
+	{
+		role: "Front-End Developer",
+		company: "Github",
+		companylogo: "/img/icons/common/github.svg",
+		date: "May 2017 – May 2018",
+		desc: "Lorem ipsum dolor sit amet, consefdctetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	},
+	{
+		role: "Software Engineer Intern",
+		company: "Airbnb",
+		companylogo: "/img/icons/common/airbnbLogo.png",
+		date: "Jan 2015 – Sep 2015",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	},
+];
+
+export const projects = [
+	{
+		name: "DiveThru",
+		desc: `Meditation application with firebase database in-app-purchase. Session wise meditation with bundles. Monthly, Yearly and Lifetime access are available. First 10 days are free to use, after that users need to purchase it.
+	
+	React-Native
+	Firebase
+	In-App purchase`,
+		link: "https://apps.apple.com/CA/app/id1383605874?mt=8"
+	},
+	{
+		name: "Teachers app",
+		desc: `The app for the teachers attendance system for this use the expo project. The teachers take selfies after coming in the class and take attendance for them and make an entry in the database from API.
+	
+	React-Native
+	Redux
+	Axios`,
+	},
+	{
+		name: "Avtar Image Generator (Open source in Node.js)",
+		desc: "Easy to Use & Focus on What Matters. Avrat Creator has a simple-to-use API with no limiting or login. Just write a name.",
+		github: "https://github.com/work4m/font-avtar-creator",
+		link: "https://font-avtar-creator.vercel.app/"
+	},
+	{
+		name: "Murabbo App",
+		desc: `The User can create the game, with contest has multiple rounds withmultiple categories with multiple round types and multiple game types Basically, there are three types of game types: question, words, and images at the create time host which set the timer for every game type. at the end of time, the next question will be arriving on the screen.
+	
+	React-Native:
+	Redux, WebRTC & React-Native-Peer
+	
+	React.js:
+	WebRTC & Peer.js
+	
+	Node.js:
+	Peer.js & Socket.io`,
+		// link: "https://apps.apple.com/in/app/evdc-charging-map/id1570309872"
+	},
+	{
+		name: "EDVC (Vehicle Charging app)",
+		desc: `The EVDC app will directly connect to the charging stations and let users exchange tokens for charging.
+	
+	React Native
+	Stripe`,
+		link: "https://apps.apple.com/in/app/evdc-charging-map/id1570309872"
+	},
+	{
+		name: "peepBook",
+		desc: `Keep notes and share to anywhere.
+	
+	React Native
+	- In app purchase
+	- Google AdMob
+	- keep access from any device with same user
+	- Share notes from key and password
+	- Sync with firebase database cloud with any device`,
+		// github: "https://github.com/1BitSpark100",
+		link: "https://play.google.com/store/apps/details?id=com.peepbook",
+	},
+	{
+		name: "KheloGame",
+		desc: `Register with apis and pay for a PUBG game. Payment purchase from Cashfree payment gateway.
+	
+	React-Native:
+	
+	Node.js: KeyStone.js, Mongodb.`,
+	},
+	{
+		name: "Flight Jester",
+		desc: `The Flight jester app has entered the id of their registration and they get all data by sorting with a dynamic component array. They get data of leave flight information return flight information pick up car information and Hotel pick up and leaving time. And if some itinerary is out of time then it will disappear from the list. Session wise meditation with bundles.
+	
+	React-Native
+	Redux
+	Axios`,
+		// link: "https://apps.apple.com/in/app/evdc-charging-map/id1570309872"
+	},
+	{
+		name: "Social Distance App",
+		desc: `Make sure users notify them to maintain social distance
+	
+	AWS:
+	Lambda function with node.js.
+	Cognito for application trigger events.
+	API gateway for call API lambda proxy.`,
+		// link: "https://apps.apple.com/in/app/evdc-charging-map/id1570309872"
+	},
+	{
+		name: "TFG_Wifi_Grill_App",
+		desc: `Manage IoT devices from mobile. Like Temperature increase/decrease. Device turn off/on.
+
+	React-Native:
+	Reduxsauce, AppSync, Cognito and Bluetooth manager for manage
+	bluetooth device from mobile
+
+	React.js:
+	AppSync, Cognito, Bootstrap, Core-UI
+
+	Node.js:
+	Lambda, AppSync, Cognito, API gateway`,
+		link: "https://play.google.com/store/apps/details?id=com.easyconnect.tfg&hl=en_SG&gl=US"
+	},
+	{
+		name: "Quokka App (School Exam App)",
+		desc: `School exam practice app with the use of MobX state management. Result, self-correction, Exam finished on given time.
+
+	React-Native:
+	MobX`,
+		// link: "https://apps.apple.com/in/app/evdc-charging-map/id1570309872"
+	},
+	{
+		name: "Pendlr",
+		desc: `Pendlr is an app service that seamlessly connects people offering and requesting ridesharing and carpooling to support everyday transport needs.
+	
+	Andoid
+	Kotlin
+	SQlite`,
+		link: "https://play.google.com/store/apps/details?id=app.uniteit.pendlr"
+	},
+	{
+		name: "FitMixPro",
+		desc: `The music app exclusively for Fitness Professionals.
+	
+	Flutter
+	Android/iOS
+	
+	PHP
+	MySQL`,
+		link: "https://play.google.com/store/apps/details?id=app.uniteit.pendlr"
+	},
+];
+
+export const feedbacks = [
+	{
+		name: "Tatva Shah (Canada)",
+		feedback:
+			"He's excellent with communication and good problem solver. Would highly recommend him.",
+	},
+	{
+		name: "Emil Lest (Russia)",
+		feedback:
+			"Thank you so much for your work I recommend to go to this specialist",
+	},
+	{
+		name: "Mgr Mohanagandhirajh (Sri Lanka)",
+		feedback:
+			"He is a very good man apart from a developer. Our project had a lot of problems when I started my contract. He did all of them without simply looking at the contract. He is very talented in React native Development and I will recommend him as he will add a lot of value to you. For more  mgreasan@gmail.com thank you kurunal",
+	},
+];
