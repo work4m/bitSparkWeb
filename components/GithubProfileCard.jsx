1a28365d9198ea0442f747bfcf9461f525ed9ce6
work4m <workingformix@gmail.com>
2022-01-18 21:00:12 +0530

    iframe issue solve

--- components/GithubProfileCard.jsx
+++ components/GithubProfileCard.jsx
@@ -34,7 +34,7 @@ const GithubProfileCard = ({ prof }) => {
 							</div>
 							<br />
 							<br /> */}
-							<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdfVhAPXnNDMDIH8x7mWJO_xm4NKsotbJt6nFsUFpWcyaCXEA/viewform?embedded=true" width="100%" height="800" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+							<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdfVhAPXnNDMDIH8x7mWJO_xm4NKsotbJt6nFsUFpWcyaCXEA/viewform?embedded=true" width="100%" height="800" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
 							{/* <SocialLinks /> */}
 						</Col>
 					</Row>
